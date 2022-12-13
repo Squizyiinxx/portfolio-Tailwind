2ebcd776144efc2416d4d4133202f1f31f036234
Squizyiinxx <wowmr51@gmail.com>
2022-12-13 16:08:17 +0700

    update features

--- public/script.js
+++ public/script.js
@@ -13,3 +13,31 @@ window.onscroll = function () {
 
     let nav = window.pageYOffset > fixNav ? header.classList.add("navbar-fixed") : header.classList.remove("navbar-fixed");
 }
+
+window.addEventListener('click',function(e) {
+   if(e.target != humburger && e.target != navLink){
+    humburger.classList.remove("hamburger-active");
+    navLink.classList.add('hidden')
+   } 
+});
+
+const darkMode = document.getElementById('check');
+const html = document.querySelector('html');
+
+darkMode.addEventListener('click', function(){
+   if(darkMode.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark'
+   }else{
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+   }
+})
+
+
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+   darkMode.checked = true;
+} else {
+  darkMode.checked = false;
+}
+ 
